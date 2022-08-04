4be59456c10d25ed8b8a58692d8f4df786353809
Filip Jakab <jakabfilip.fj@gmail.com>
2022-08-04 02:58:18 +0200

    Added new useful helpers

---
+++ src/helpers/object-helpers.js
@@ -0,0 +1,10 @@
+﻿export function anyKeyTruthy(t) {
+	return Object.keys(t).some(k => t[k])
+}
+
+export function removeTemplateTimestamps(obj) {
+	delete obj.createdBy
+	delete obj.created
+	delete obj.modifiedBy
+	delete obj.modified
+}
