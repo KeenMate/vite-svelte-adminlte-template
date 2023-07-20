5df1ed176809560a901ee3ee042e95b0d34cdb6a
Filip Jakab <jakabfilip.fj@gmail.com>
2023-07-21 00:30:27 +0200

    Added BoolIcon.svelte for rendering true/false value using icons

---
+++ src/lib/components/common/ui/icons/BoolIcon.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let value = false
+	export let style = null
+	export let noColors = false
+</script>
+
+{#if value}
+	<i
+		class="fas fa-check{style ? '-' + style : ''} fa-fw"
+		class:text-success={!noColors}
+	/>
+{:else}
+	<i
+		class="fas fa-times{style ? '-' + style : ''} fa-fw"
+		class:text-danger={!noColors}
+	/>
+{/if}
