99df6896cccaca890d929073c762c38b14151d48
Filip Jakab <jakabfilip.fj@gmail.com>
2023-07-21 00:36:53 +0200

    New version of BoolIcon.svelte

--- src/lib/components/common/ui/icons/BoolIcon.svelte
+++ src/lib/components/common/ui/icons/BoolIcon.svelte
@@ -1,17 +1,26 @@
 <script>
 	export let value = false
+
+	/**
+	 * Used to alter the rendering style of icons
+	 * @type {"square" | "circle" | null}
+	 */
 	export let style = null
 	export let noColors = false
+
+	let icon,
+		fullIconClass,
+		color
+
+	$: icon = value
+		&& "fa-check"
+		|| "fa-times"
+	$: fullIconClass = style ? `${icon}-${style}` : icon
+	$: color = noColors
+		? ""
+		: value
+		&& "text-success"
+		|| "text-danger"
 </script>
 
-{#if value}
-	<i
-		class="fas fa-check{style ? '-' + style : ''} fa-fw"
-		class:text-success={!noColors}
-	/>
-{:else}
-	<i
-		class="fas fa-times{style ? '-' + style : ''} fa-fw"
-		class:text-danger={!noColors}
-	/>
-{/if}
+<i class="fas {fullIconClass} {color} fa-fw" />
